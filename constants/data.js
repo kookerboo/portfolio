bfc11253788b50fd80e0f9d7c9e5f5f90895e6d3
Claire Marie <claire.brockelbank@gmail.com>
2023-06-01 10:02:05 -0400

    Update portfolio site

---
+++ constants/data.js
@@ -0,0 +1,93 @@
+const userData = {
+  githubUsername: "kookerboo",
+  name: "Claire Johnston",
+  designation: "Full-Stack Developer",
+  avatarUrl: "/avatar.png",
+  email: "hello@clairejohnston.ca",
+  phone: "+19053990575",
+  address: "Ontario, Canada",
+  projects: [
+    {
+      title: "Tailwind Master Kit",
+      link: "https://tailwindmasterkit.com",
+      imgUrl: "/tmk.jpg",
+    },
+    {
+      title: "PlaceholderTech",
+      link: "https://placeholdertech.in",
+      imgUrl: "/placeholdertech.png",
+    },
+    {
+      title: "Portfolio",
+      link: "https://manuarora.in",
+      imgUrl: "/portfolio.png",
+    },
+    {
+      title: "CovidRescue",
+      link: "https://covidrescue.co.in",
+      imgUrl: "/covidrescue.png",
+    },
+    {
+      title: "Algochurn",
+      link: "https://algochurn.com",
+      imgUrl: "/algochurn.png",
+    },
+    {
+      title: "Playground",
+      link: "https://play.placeholdertech.in",
+      imgUrl: "/playground.png",
+    },
+    {
+      title: "Feedmeback",
+      link: "https://feedmeback.vercel.app",
+      imgUrl: "/feedmeback.png",
+    },
+    {
+      title: "Devmedium",
+      link: "https://devmedium.vercel.app",
+      imgUrl: "/devmedium.png",
+    },
+  ],
+  about: {
+    title:
+      "I'm a software developer that loves building products and web applications that impact millions of lives",
+    description: [
+      "I've been developing full-stack application since I was 10 years old. I didn't know what full-stack meant at that time because the term was not coined back then. I'm so out of content right now that I'm literally crying writing this text since lorem text is so 2012 now.",
+      "After learning HTML and struggling with CSS, I came up with a brilliant idea of using bootstrap so that I don't have to style everything by myself and - for obvious reasons - if you knew bootstrap, you were cool.",
+      "But now, I'm a legend. I've been coding in React, Angular, Vue, Svelte, Node.js, Express, Fastify, MongoDB, MySQL, Bootstrap, Tailwind, C, C++, C#, Rust, Haskell, Perl, PHP, Assembly, Logo and YouDontKnowShit programming language etc to name a few. Although I barely know the syntax (Psst, Stack overflow!), I consider myself a Ninja developer I'm laughing right now.",
+    ],
+    currentProject: "Tailwind Master Kit",
+    currentProjectUrl: "https://tailwindmasterkit.com",
+  },
+  experience: [
+    {
+      title: "Senior Developer",
+      company: "Atlas Van Lines Canada Ltd.",
+      year: "2014 - 2022",
+      companyLink: "https://atlasvanlines.ca",
+      desc: [
+      "Lead developer responsible for numerous migration projects, ensuring smooth transitions and timely delivery of results.  ",
+      "Performed end-to-end software development activities including analysis, design, coding, debugging, and testing of various applications.",
+      ],
+    },
+    {
+      title: "Computer Programmer",
+      company: "Sheridan College",
+      year: "2011 - 2013",
+      companyLink: "https://www.sheridancollege.ca/programs/computer-programming",
+      desc: "Obtained a Computer Programmer Diploma.  Courses included Java, C# and .NET, Web Development, Database design, Systems analysis and design, Network communications and configurations."
+    },
+  ],
+  resumeUrl:
+    "https://drive.google.com/file/d/1-Y55aE5bWKHKxPUjcUVG6_Xyzm90Q7_c/view?usp=sharing",
+  socialLinks: {
+    instagram: "https://instagram.com/kookerboo",
+    // twitter: "https://twitter.com/mannupaaji",
+    linkedin: "https://linkedin.com/in/clairebrockelbank",
+    github: "https://github.com/kookerboo",
+    // facebook: "https://facebook.com/",
+    // peerlist: "https://peerlist.io/manuarora",
+  },
+};
+
+export default userData;
